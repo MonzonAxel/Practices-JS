2dcf5e28428d9ef424196c34c521da06ab54d783
MonzonAxel <axel.monzon15@hotmail.com>
2024-08-11 20:36:52 -0300

    added forecast funcionality

--- weatherApp/js/script.js
+++ weatherApp/js/script.js
@@ -1,21 +1,111 @@
 
-const API_KEY = "4d3fb563f0f4c0f4752ed1adc41ace16"
+const API_KEY = "52c5ab5d2a1b49658de194604241108"
 
-// Clima promedio 
-fetch(`https://api.openweathermap.org/data/2.5/weather?q=Argentina&appid=${API_KEY}&units=metric&lang=es`)
-.then(res => res.json())
-.then(data => {
-    console.log(data)
+const selectors = {
+    input:document.querySelector(".input"),
+    btn:document.querySelector(".btn"),
+    view:document.querySelector(".fa-eye"),
+    container:document.querySelector(".container-extend")
+}
+
+const weatherPrincipal = {
+    img:document.querySelector(".img"),
+    temperature:document.querySelector(".temperature"),
+    nation:document.querySelector(".weather-info-nation"),
+    region:document.querySelector(".weather-info-region"),
+    description:document.querySelector(".description"),
+    wind:document.querySelector(".weather-info-wind"),
+    humidity:document.querySelector(".weather-info-humidity"),
+    clouds:document.querySelector(".weather-info-clouds"),
+    name:document.querySelector(".span-city")
+}
+
+const fetchWeather = (value = "Argentina") =>  {
+    fetch(`https://api.weatherapi.com/v1/current.json?key=${API_KEY}&q=${value}&lang=es`)
+    .then(res => res.json())
+    .then(data => {
+        displayWeather(data)
+    })
+    .catch(error =>{
+        console.log(error)
+    }) 
+}
+
+const displayWeather = (data) => {
+
+    const {location:{name,country,region},current:{temp_c, condition:{icon,text},wind_kph,humidity,cloud}} = data ;
+
+    weatherPrincipal.img.src = icon
+    weatherPrincipal.temperature.textContent = `${temp_c} °C`
+    weatherPrincipal.nation.textContent = `${country}`
+    weatherPrincipal.region.textContent = `${region}`
+    weatherPrincipal.description.textContent = `${text}`
+    weatherPrincipal.wind.textContent = `Viento: ${wind_kph} km/h`
+    weatherPrincipal.humidity.textContent=`Humedad: ${humidity}%`
+    weatherPrincipal.clouds.textContent=` Nubes : ${cloud}%`
+    weatherPrincipal.name.textContent = name
+
+}
+
+const fetchForescast = (value = "Argentina") =>{
+    fetch(`https://api.weatherapi.com/v1/forecast.json?key=${API_KEY}&q=${value}&days=3&lang=es`)
+    .then(res => res.json())
+    .then(data => {
+        displayForecast(data)
+    })
+    .catch(error =>{
+        console.log(error)
+    })
+}
+
+const displayForecast = (data) =>{
+    const {forecast:{forecastday}} = data   
+
+    selectors.container.innerHTML =""
+
+    for(let i=0; i<forecastday.length; i++){
+    const day = forecastday[i]
+    console.log(day)
+
+    const element = `
+                    <article class="days-extend">
+                    <p class="extend-day">Dia</p>
+                    <p class="extend-date">${day.date}</p>
+                    <img class="extend-img" src="${day.day.condition.icon}" alt="Icon">
+                    <div class="weather-info-extend">
+                        <div class="flex">
+                            <i class="fa-solid fa-temperature-three-quarters"></i>
+                            <p>T. Max :${day.day.maxtemp_c}</p>
+                        </div>
+                        <div class="flex">
+                            <i class="fa-solid fa-temperature-empty"></i>
+                            <p>T. Min :${day.day.mintemp_c} </p>
+                        </div>
+                        <div class="flex-eye">
+                            <i class="fa-solid fa-eye"></i>
+                        </div>
+                    </article> `
+
+        selectors.container.insertAdjacentHTML("beforeend",element)
+   }
+}
+
+
+selectors.btn.addEventListener("click", () =>{
+   const query = selectors.input.value
+   fetchWeather(query)
+   fetchForescast(query)
 })
 
+fetchWeather()
 
-// Clima de los proximos 5 dias en distintas horas.
+fetchForescast()
 
-fetch(`https://api.openweathermap.org/data/2.5/forecast?q=Argentina&appid=${API_KEY}&units=metric&lang=es`)
-.then(res => res.json())
-.then(data => {
-    console.log(data)
-})
+
+
+
+
+
 
 // const ctx = document.getElementById('myChart');
 
