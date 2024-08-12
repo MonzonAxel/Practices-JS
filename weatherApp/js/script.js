6ff9e1b0dcebb8e3aab2b3fb8a863703404b3f73
MonzonAxel <axel.monzon15@hotmail.com>
2024-08-11 23:41:39 -0300

    added charts.js

--- weatherApp/js/script.js
+++ weatherApp/js/script.js
@@ -17,14 +17,18 @@ const weatherPrincipal = {
     wind:document.querySelector(".weather-info-wind"),
     humidity:document.querySelector(".weather-info-humidity"),
     clouds:document.querySelector(".weather-info-clouds"),
-    name:document.querySelector(".span-city")
+    name:document.querySelector(".span-city"),
+    descriptionExtend:document.querySelector(".extend-description")
 }
 
 const fetchWeather = (value = "Argentina") =>  {
-    fetch(`https://api.weatherapi.com/v1/current.json?key=${API_KEY}&q=${value}&lang=es`)
+    fetch(`https://api.weatherapi.com/v1/forecast.json?key=${API_KEY}&q=${value}&days=3&lang=es`)
+
     .then(res => res.json())
     .then(data => {
         displayWeather(data)
+        displayForecast(data)
+        displayChart(data)
     })
     .catch(error =>{
         console.log(error)
@@ -47,39 +51,36 @@ const displayWeather = (data) => {
 
 }
 
-const fetchForescast = (value = "Argentina") =>{
-    fetch(`https://api.weatherapi.com/v1/forecast.json?key=${API_KEY}&q=${value}&days=3&lang=es`)
-    .then(res => res.json())
-    .then(data => {
-        displayForecast(data)
-    })
-    .catch(error =>{
-        console.log(error)
-    })
-}
+let weatherChart = null;
 
 const displayForecast = (data) =>{
+
     const {forecast:{forecastday}} = data   
 
     selectors.container.innerHTML =""
 
     for(let i=0; i<forecastday.length; i++){
     const day = forecastday[i]
-    console.log(day)
+
+    const dayFormatted = day.date.split("-").reverse().join("/")
+    const dayName = getDayName(day.date).charAt(0).toUpperCase().concat(getDayName(day.date).slice(1))
+    
+    
 
     const element = `
                     <article class="days-extend">
-                    <p class="extend-day">Dia</p>
-                    <p class="extend-date">${day.date}</p>
+                    <p class="extend-day">${dayName}</p>
+                    <p class="extend-date">${dayFormatted}</p>
                     <img class="extend-img" src="${day.day.condition.icon}" alt="Icon">
+                    <p class="extend-description">${day.day.condition.text}</p>
                     <div class="weather-info-extend">
                         <div class="flex">
                             <i class="fa-solid fa-temperature-three-quarters"></i>
-                            <p>T. Max :${day.day.maxtemp_c}</p>
+                            <p class="temp">T.Max: ${day.day.maxtemp_c} °C</p>
                         </div>
                         <div class="flex">
                             <i class="fa-solid fa-temperature-empty"></i>
-                            <p>T. Min :${day.day.mintemp_c} </p>
+                            <p class="temp">T.Min: ${day.day.mintemp_c} °C </p>
                         </div>
                         <div class="flex-eye">
                             <i class="fa-solid fa-eye"></i>
@@ -90,40 +91,74 @@ const displayForecast = (data) =>{
    }
 }
 
+const getDayName = (dateString) => {
+    const date = new Date(dateString);
+    const options = { weekday: 'long' };
+    return new Intl.DateTimeFormat('es-ES', options).format(date);
+}
+
+const displayChart = (data) => {
+    const ctx = document.getElementById("myChart")
+
+    // Destruir el gráfico anterior si existe
+    if (weatherChart) {
+        weatherChart.destroy();
+    }
+    
+    const {forecast:{forecastday}} = data 
+
+        console.log(forecastday)
+        const labels = forecastday.map(day => getDayName(day.date));
+        console.log(labels)
+        const maxTemps = forecastday.map(day => day.day.maxtemp_c);
+        console.log(maxTemps)
+        const minTemps = forecastday.map(day => day.day.mintemp_c);
+        console.log(minTemps)
+
+        weatherChart = new Chart(ctx, {
+            type: 'line',
+            data: {
+                labels: labels,
+                datasets: [
+                    {
+                        label: 'Temperatura Máxima (°C)',
+                        data: maxTemps,
+                        borderColor: 'rgba(255, 99, 132, 1)',
+                        backgroundColor: 'rgba(255, 99, 132, 0.2)',
+                        fill: true,
+                        tension: 0.1
+                    },
+                    {
+                        label: 'Temperatura Mínima (°C)',
+                        data: minTemps,
+                        borderColor: 'rgba(54, 162, 235, 1)',
+                        backgroundColor: 'rgba(54, 162, 235, 0.2)',
+                        fill: true,
+                        tension: 0.1
+                    }
+                ]
+            },
+            options: {
+                scales: {
+                    y: {
+                        beginAtZero: false
+                    }
+                }
+            }
+        });
+    
+}
+
 
 selectors.btn.addEventListener("click", () =>{
    const query = selectors.input.value
    fetchWeather(query)
-   fetchForescast(query)
 })
 
-fetchWeather()
+selectors.input.addEventListener("keydown", (e) =>{
+    const query = selectors.input.value
+    if (e.key === "Enter") fetchWeather(query) 
+})
 
-fetchForescast()
+ fetchWeather()
 
-
-
-
-
-
-
-// const ctx = document.getElementById('myChart');
-
-//   new Chart(ctx, {
-//     type: 'line',
-//     data: {
-//       labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
-//       datasets: [{
-//         label: '# of Votes',
-//         data: [12, 19, 3, 5, 2, 3],
-//         borderWidth: 1
-//       }]
-//     },
-//     options: {
-//       scales: {
-//         y: {
-//           beginAtZero: true
-//         }
-//       }
-//     }
-//   });
